0043efe4084b5abfbabaf464c02bd4a7486f1072
AskarShigabiev <askarshigabiev@yandex.ru>
2024-04-07 05:24:06 +0300

    Ревью_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, removeCard) {
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+const cardImage = cardElement.querySelector('.card__image');
+const cardTitle = cardElement.querySelector('.card__title');
+cardImage.src = cardData.link;
+cardImage.alt = cardData.name;
+cardTitle.textContent = cardData.name;
+
+const deleteButton = cardElement.querySelector('.card__delete-button');
+deleteButton.addEventListener('click', () => {
+removeCard(cardElement);
+});
+
+return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item){
+const cardElement = createCard(item, deleteCard);
+placesList.append(cardElement);
+});
